53eaec59835be52ec8d821ecf5dfd3d44f370f37
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-05-19 09:30:37 -0300

    Instalando framework mui

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+import { Container, Grid, Typography } from "@mui/material";
+
 function App() {
   return (
-    <>
-      <h1>App Noticias</h1>
-    </>
+    <Container>
+      <header>
+        <Typography
+          align="center"
+          marginY={5}
+          sx={{ fontWeight: "bold" }}
+          component="h1"
+          variant="h3"
+        >
+          Buscador de Noticias
+        </Typography>
+      </header>
+    </Container>
   );
 }
 
