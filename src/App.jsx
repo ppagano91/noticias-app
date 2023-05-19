f25668cebd46bac2d8db6ecad7accbec7fd3bf84
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-05-19 09:46:19 -0300

    Creando formulario

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Container, Grid, Typography } from "@mui/material";
+import Formulario from "./components/Formulario";
 
 function App() {
   return (
@@ -14,6 +15,16 @@ function App() {
           Buscador de Noticias
         </Typography>
       </header>
+      <Grid
+        container
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Grid item xs={12} md={6} lg={4}>
+          <Formulario />
+        </Grid>
+      </Grid>
     </Container>
   );
 }
