65411ff2f53225702071ebcafd51a744878ee242
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-05-22 15:21:19 -0300

    Consultando la API de noticias

--- src/context/NoticiasProvider.jsx
+++ src/context/NoticiasProvider.jsx
@@ -1,9 +1,22 @@
 import { useState, useEffect, createContext } from "react";
+import axios from "axios";
 
 const NoticiasContext = createContext();
 
 const NoticiasProvider = ({ children }) => {
   const [categoria, setCategoria] = useState("general");
+  const [noticias, setNoticias] = useState([]);
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      const API_KEY = import.meta.env.VITE_API_KEY;
+      const URL = `https://newsapi.org/v2/top-headlines?country=ar&category=${categoria}&apiKey=${API_KEY}`;
+      const { data } = await axios(URL);
+      console.log(data);
+      setNoticias(data.articles);
+    };
+    consultarAPI();
+  }, [categoria]);
 
   const handleChangeCategoria = (e) => {
     setCategoria(e.target.value);
